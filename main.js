3a8dbf2ab887c92274900764e769cc71226dcdbf
Almas shaikh <128784915+almas7202@users.noreply.github.com>
2024-03-04 21:29:59 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ $(document).ready(()=>{
     const sidebaar = $('#sidebaar')
    
 
-    fetch('Task.json')
+    fetch('TaskJson.json')
     .then(response => response.json())
     .then(data => {
      
@@ -17,42 +17,40 @@ $(document).ready(()=>{
         Object.keys(data).forEach(key => {
             const navItem = $('<a> </a>').text(key).attr("href",'#').attr("style","margin-left:25px;").click(()=>{
                 // console.log(data[key]);
-                createSidebaar(data[key])
+                createSidebar(data[key])
             })
             navbar.append(navItem)
         })
     }
 
-    function createSidebaar(data){
-        $(sidebaar).empty("")
-        console.log(data);
-        Object.keys(data).forEach((module) => {
-            // console.log(data)
-            const moduleItems = $('<a> </a> <br>').text(module).append('module').attr({class:"btn btn-secondary dropdown-toggle"}).click(()=>{
-                data[module].forEach(item=>{
-                    Object.keys(item).forEach(subItem=>{
-                        console.log(subItem);
-                        $('<li> </li').text(subItem).attr({class:"dropdown-item"}).append(sidebaar)
-                    })
-                })
-            })
-            sidebaar.append(moduleItems)
-          
-            // data[module].forEach(item=>{
-            //     Object.keys(item).forEach(subitem=>{
-            //         console.log(subitem);
-
-            //         const subItem = $("<button> </button>").text(Object.keys.subItem).addClass("btn btn-secondary").click(()=>{
-            //             console.log(Object.keys.subItem);
-            //         }).append(dropdown)
-            //         sidebaar.append(subItem)
-            //     })
-            // })
-
-        })
-        
+    function createSidebar(data) {
+        $(sidebaar).empty();
+        Object.keys(data).forEach(module => {
+            const moduleContainer = $("<ul>").addClass("module-container");
+            const moduleList = $("<li>").text(module).addClass("module-heading");
+            const subItemsList = $("<ul>").addClass("sub-items-list");
+            
+            data[module].forEach(item => {
+                Object.keys(item).forEach(subItem => {
+                    const subItemLink = $("<a>").text(subItem).addClass("sub-item-link").attr("href", item[subItem]);
+                    const subItemListItem = $("<li>").append(subItemLink);
+                    subItemsList.append(subItemListItem);
+                });
+            });
+    
+            moduleList.click(function() {
+                subItemsList.toggle();
+            });
+    
+            moduleContainer.append(moduleList, subItemsList);
+            
+            sidebaar.append(moduleContainer);
+        });
     }
+    
+    
+    
+    
 
    
 })
-
